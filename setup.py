4886277a8f110bf43888826a5aa76d6036812520
Flowshu <floowshu@gmail.com>
2023-07-14 12:07:43 +0200

    add configs module to setup

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='maskgit',
       version='1.0',
-      packages=['maskgit', 'maskgit.libml', 'maskgit.nets'],
+      packages=['maskgit', 'maskgit.configs', 'maskgit.libml', 'maskgit.nets'],
      )
